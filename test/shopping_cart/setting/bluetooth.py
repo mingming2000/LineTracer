feafada002095e6e1b98f8d65fdb4f4571fe28ce
mingming2000 <alsrud1228@korea.ac.kr>
2024-01-27 21:12:03 -1000

    setting files for accepting info. and manipulating motor/servo-motor

---
+++ test/shopping_cart/setting/bluetooth.py
@@ -0,0 +1,13 @@
+def initializing(return_dict):
+    
+    pass
+    
+    # Return True if detecting user works.
+    # Return User degree
+    return_dict[initial_degree] = ''
+
+def calculate_degree(return_dict):
+
+    pass
+
+    return_dict[current_degree] = ''
---
+++ test/shopping_cart/setting/camera.py
@@ -0,0 +1,2 @@
+def calculate_degree():
+    pass
